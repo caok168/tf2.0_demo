fa71ce39420158ec4777143cd1a30297b17190f7
kai.cao <kai.cao@lynxi.com>
2020-12-07 11:18:18 +0800

    add chapter_7 demo and datasets

---
+++ chapter_7/embedding_rnn.py
@@ -0,0 +1,156 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+
+from tensorflow import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
+
+imdb = keras.datasets.imdb
+vocab_size = 10000
+index_from = 3
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=vocab_size, index_from=index_from)
+
+print(train_data[0], train_labels[0])
+print(train_data.shape, train_labels.shape)
+print(len(train_data[0]), len(train_data[1]))
+
+print(test_data.shape, test_labels.shape)
+
+word_index = imdb.get_word_index()
+print(len(word_index))
+print(word_index)
+
+word_index = {k:(v+3) for k, v in word_index.items()}
+
+word_index['<PAD>'] = 0
+word_index['<START>'] = 1
+word_index['<UNK>'] = 2
+word_index['END'] = 3
+
+reverse_word_index = dict([(value, key) for key, value in word_index.items()])
+
+
+def decode_review(text_ids):
+    return ' '.join([reverse_word_index.get(word_id, "<UNK>") for word_id in text_ids])
+
+
+decode_review(train_data[0])
+
+max_length = 500
+
+train_data = keras.preprocessing.sequence.pad_sequences(
+    train_data,  # list of list
+    value=word_index['<PAD>'],
+    padding='post',
+    maxlen=max_length)
+
+test_data = keras.preprocessing.sequence.pad_sequences(
+    test_data,  # list of list
+    value=word_index['<PAD>'],
+    padding='post',
+    maxlen=max_length)
+
+print(train_data[0])
+
+embedding_dim = 16
+batch_size = 128
+
+single_rnn_model = keras.models.Sequential([
+    # 1. define matrix: [vocab_size, embedding_dim]
+    # 2. [1, 2, 3, 4..], max_length * embedding_dim
+    # 3. batch_size * max_length * embedding_dim
+    keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    keras.layers.SimpleRNN(units=64, return_sequences=False),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid'),
+])
+
+single_rnn_model.summary()
+single_rnn_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+histroy = single_rnn_model.fit(train_data, train_labels,
+                               epochs=30,
+                               batch_size=batch_size,
+                               validation_split=0.2)
+
+
+def plot_learning_curves(history, label, epochs, min_value, max_value):
+    data = {}
+    data[label] = histroy.history[label]
+    data['val_'+label] = histroy.history['val_'+label]
+    pd.DataFrame(data).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.axis([0, epochs, min_value, max_value])
+    plt.show()
+
+
+plot_learning_curves(histroy, 'accuracy', 30, 0, 1)
+plot_learning_curves(histroy, 'loss', 30, 0, 1)
+
+single_rnn_model.evaluate(test_data, test_labels, batch_size=batch_size)
+
+
+
+embedding_dim = 16
+batch_size = 128
+
+model = keras.models.Sequential([
+    # 1. define matrix: [vocab_size, embedding_dim]
+    # 2. [1, 2, 3, 4..], max_length * embedding_dim
+    # 3. batch_size * max_length * embedding_dim
+    keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    keras.layers.Bidirectional(keras.layers.SimpleRNN(units=64, return_sequences=True)),
+    keras.layers.Bidirectional(keras.layers.SimpleRNN(units=64, return_sequences=False)),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid'),
+])
+
+model.summary()
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+histroy = model.fit(train_data, train_labels,
+                    epochs=30,
+                    batch_size=batch_size,
+                    validation_split=0.2)
+
+plot_learning_curves(histroy, 'accuracy', 30, 0, 1)
+plot_learning_curves(histroy, 'loss', 30, 0, 1)
+
+
+
+embedding_dim = 16
+batch_size = 128
+
+bi_rnn_model = keras.models.Sequential([
+    # 1. define matrix: [vocab_size, embedding_dim]
+    # 2. [1, 2, 3, 4..], max_length * embedding_dim
+    # 3. batch_size * max_length * embedding_dim
+    keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    keras.layers.Bidirectional(keras.layers.SimpleRNN(units=32, return_sequences=False)),
+    keras.layers.Dense(32, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid'),
+])
+
+bi_rnn_model.summary()
+bi_rnn_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+histroy = bi_rnn_model.fit(train_data, train_labels,
+                           epochs=30,
+                           batch_size=batch_size,
+                           validation_split=0.2)
+
+plot_learning_curves(histroy, 'accuracy', 30, 0, 1)
+plot_learning_curves(histroy, 'loss', 30, 0, 1.5)
+
+bi_rnn_model.evaluate(test_data, test_labels, batch_size=batch_size)
---
+++ chapter_7/test_generation.py
@@ -0,0 +1,183 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+
+from tensorflow import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
+
+input_filepath = './shakespeare.txt'
+text = open(input_filepath, "r").read()
+
+print(len(text))
+print(text[0:100])
+
+# 1. generate vocab
+# 2. build mapping char->id
+# 3. data -> id_data
+# 4. abcd -> bcd<eos>
+
+vocab = sorted(set(text))
+print(len(vocab))
+print(vocab)
+
+char2idx = {char:idx for idx, char in enumerate(vocab)}
+print(char2idx)
+
+idx2char = np.array(vocab)
+print(idx2char)
+
+text_as_int = np.array([char2idx[c] for c in text])
+print(text_as_int[0:10])
+print(text[0:10])
+
+
+def split_input_target(id_text):
+    """
+    abcde -> abcd, bcde
+    :param id_text:
+    :return:
+    """
+    return id_text[0:-1], id_text[1:]
+
+
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+seq_length = 100
+seq_dataset = char_dataset.batch(seq_length + 1, drop_remainder=True)
+
+for ch_id in char_dataset.take(2):
+    print(ch_id, idx2char[ch_id.numpy()])
+
+for seq_id in seq_dataset.take(2):
+    print(seq_id)
+    print(repr(''.join(idx2char[seq_id.numpy()])))
+
+seq_dataset = seq_dataset.map(split_input_target)
+
+for item_input, item_output in seq_dataset.take(2):
+    print(item_input.numpy())
+    print(item_output.numpy())
+
+batch_size = 64
+buffer_size = 10000
+
+seq_dataset = seq_dataset.shuffle(buffer_size).batch(batch_size, drop_remainder=True)
+
+vocab_size = len(vocab)
+embedding_dim = 256
+rnn_units = 1024
+
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = keras.models.Sequential([
+        keras.layers.Embedding(vocab_size, embedding_dim,
+                               batch_input_shape=[batch_size, None]),
+        keras.layers.SimpleRNN(units=rnn_units,
+                               return_sequences=True),
+        keras.layers.Dense(vocab_size)
+    ])
+    return model
+
+
+model = build_model(
+    vocab_size=vocab_size,
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units,
+    batch_size=batch_size)
+
+model.summary()
+
+for input_example_batch, target_example_batch in seq_dataset.take(1):
+    example_batch_predictions = model(input_example_batch)
+    print(example_batch_predictions.shape)
+
+# random sampling.
+# greedy, random.
+sample_indices = tf.random.categorical(logits=example_batch_predictions[0],
+                                       num_samples=1)
+print(sample_indices)
+# (100, 65) -> (100, 1)
+
+sample_indices = tf.squeeze(sample_indices, axis=-1)
+print(sample_indices)
+
+print("Input: ", repr("".join(idx2char[input_example_batch[0]])))
+print()
+print("Output: ", repr("".join(idx2char[target_example_batch[0]])))
+print()
+print("Predictions: ", repr("".join(idx2char[sample_indices])))
+
+
+def loss(labels, logits):
+    return keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+
+model.compile(optimizer='adam', loss=loss)
+example_loss = loss(target_example_batch, example_batch_predictions)
+print(example_loss.shape)
+print(example_loss.numpy().mean())
+
+output_dir = "./text_generation_checkpoints"
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+checkpoint_prefix = os.path.join(output_dir, 'ckpt_{epoch}')
+checkpoint_callback = keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+epochs = 100
+history = model.fit(seq_dataset, epochs=epochs, callbacks=[checkpoint_callback])
+
+tf.train.latest_checkpoint(output_dir)
+
+
+model2 = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+model2.load_weights(tf.train.latest_checkpoint(output_dir))
+model2.build(tf.TensorShape([1, None]))
+# start ch sequence A,
+# A -> model -> b
+# A.append(b) -> B
+# B -> model -> c
+# b.append(c) -> C
+# C(Abc) -> model -> ...
+model2.summary()
+
+
+def generate_text(model, start_string, num_generate=1000):
+    input_eval = [char2idx[ch] for ch in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    text_generated = []
+    model.reset_states()
+
+    for _ in range(num_generate):
+        # 1. model inference -> predictions
+        # 2. sample -> ch -> text_generated.
+        # 3. update input_eval
+
+        # predictions: [batch_size, input_eval_len, vocab_size]
+        predictions = model(input_eval)
+        # predictions: [input_eval_len, vocab_size]
+        predictions = tf.squeeze(predictions, 0)
+        # predicted_ids: [input_eval_len, 1]
+        # a b c -> b c d
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+        text_generated.append(idx2char[predicted_id])
+        # s, x -> rnn -> s', y
+        input_eval = tf.expand_dims([predicted_id], 0)
+
+    return start_string + ''.join(text_generated)
+
+
+new_text = generate_text(model2, "All: ")
+print(new_text)
