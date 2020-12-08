6ad9f3b0742d3915a983b253df110bf327b0d59c
kai.cao <kai.cao@lynxi.com>
2020-12-08 08:53:49 +0800

    add chapter_8 estimator and customized demo

---
+++ chapter_8/tf_distributed_estimator.py
@@ -0,0 +1,123 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+
+from tensorflow import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
+
+tf.debugging.set_log_device_placement(True)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+print(len(gpus))
+logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+print(len(logical_gpus))
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+(x_train_all, y_train_all), (x_test, y_test) = fashion_mnist.load_data()
+x_valid, x_train = x_train_all[:5000], x_train_all[5000:]
+y_valid, y_train = y_train_all[:5000], y_train_all[5000:]
+
+print(x_valid.shape, y_valid.shape)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(
+    x_train.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28, 1)
+x_valid_scaled = scaler.transform(
+    x_valid.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28, 1)
+x_test_scaled = scaler.transform(
+    x_test.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28, 1)
+
+
+def make_dataset(images, labels, epochs, batch_size, shuffle=True):
+    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+    if shuffle:
+        dataset = dataset.shuffle(10000)
+    dataset = dataset.repeat(epochs).batch(batch_size).prefetch(50)
+    return dataset
+
+
+batch_size = 256
+epochs = 100
+train_dataset = make_dataset(x_train_scaled, y_train, epochs, batch_size)
+
+
+model = keras.models.Sequential()
+model.add(keras.layers.Conv2D(filters=128,
+                              kernel_size=3,
+                              padding='same',
+                              activation='relu',
+                              input_shape=(28, 28, 1)))
+model.add(keras.layers.Conv2D(filters=128, kernel_size=3,
+                              padding='same',
+                              activation='relu'))
+model.add(keras.layers.MaxPool2D(pool_size=2))
+
+model.add(keras.layers.Conv2D(filters=256, kernel_size=3,
+                              padding='same',
+                              activation='relu'))
+model.add(keras.layers.Conv2D(filters=256, kernel_size=3,
+                              padding='same',
+                              activation='relu'))
+model.add(keras.layers.MaxPool2D(pool_size=2))
+
+model.add(keras.layers.Conv2D(filters=512, kernel_size=3,
+                              padding='same',
+                              activation='relu'))
+model.add(keras.layers.Conv2D(filters=512, kernel_size=3,
+                              padding='same',
+                              activation='relu'))
+model.add(keras.layers.MaxPool2D(pool_size=2))
+
+model.add(keras.layers.Flatten())
+
+model.add(keras.layers.Dense(512, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="sgd",
+              metrics=["accuracy"])
+
+strategy = tf.distribute.MirroredStrategy()
+config = tf.estimator.RunConfig(train_distribute=strategy)
+estimator = keras.estimator.model_to_estimator(model, config=config)
+
+
+model.summary()
+
+
+# history = model.fit(train_dataset,
+#                     steps_per_epoch=x_train_scaled.shape[0],  # batch_size,
+#                     epochs=10)
+
+estimator.train(input_fn=lambda: make_dataset(x_train_scaled, y_train, epochs, batch_size), max_steps=5000)
+
+
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 1)
+    plt.show()
+
+
+# plot_learning_curves(history)
+
+
+model.evaluate(x_test_scaled, y_test)
